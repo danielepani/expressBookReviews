a9d36882b16e60c9cd7893df96f8aa5a1631860b
SharkAttack86 <daniele.pani.86@gmail.com>
2024-06-12 14:44:15 +0200

    Implemented a JWT based auth

--- final_project/index.js
+++ final_project/index.js
@@ -11,12 +11,29 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const {user, password} = req.query;
+
+    if (user !== 'Daniele' || password !== '123456') {
+        return next({
+            message: 'Invalid',
+        })
+    }
+    const token = jwt.sign({user: 'Daniele'}, 'pkey1234');
+
+    return res.json({
+        token
+    })
 });
  
 const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
+
+app.use((err, req, res, next) => {
+    return res.json({
+        error: err
+    })
+})
 
 app.listen(PORT,()=>console.log("Server is running"));
