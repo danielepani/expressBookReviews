7c41c46f6df445247f7ce9460759116f61b98ed2
SharkAttack86 <daniele.pani.86@gmail.com>
2024-06-16 21:36:20 +0200

    Task 1 and 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,14 +12,17 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.json(books)
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {isbn} = req.params;
+
+  const book = books[isbn]; // assuming isbn is the key
+
+  return res.json(book);
  });
   
 // Get book details based on author
