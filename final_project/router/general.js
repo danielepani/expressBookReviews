42694273b97214decb1ab223404542e89f3c77df
SharkAttack86 <daniele.pani.86@gmail.com>
2024-06-16 22:05:58 +0200

    Task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -53,7 +53,11 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const {isbn} = req.params;
+
+  const book = books[isbn]; // assuming isbn is the key
+
+  return res.json(book.reviews);
 });
 
 module.exports.general = public_users;
